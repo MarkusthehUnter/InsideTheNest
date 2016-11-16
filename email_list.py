866ff8f6a8a372755b444ff3f83f846c64304b2e
Mark Unter <mark.unter@nestio.com>
2016-11-16 13:39:17 -0500

    Adds functionality to parse emails by attachment, need to update the hash table  based off of cksum

--- email_list.py
+++ email_list.py
@@ -3,6 +3,18 @@ import imaplib
 import email
 import time
 
+
+def grab_attached(em):
+    attachments=[]
+    for part in em.walk():
+        if part.get_content_maintype() == 'multipart': continue
+        if part.get('Content-Disposition') is None: continue
+        attachments.append(part.get_filename())
+    return attachments
+
+
+
+
 class DEinbox:
     def __init__(self,e_mail,password):
         self.e_mail=e_mail
@@ -19,7 +31,11 @@ class DEinbox:
             print('got unread emails')
             for em in unread_em:
                 em1=email.message_from_string( obj.fetch(em, '(RFC822)')[1][0][1])
-                self.unread.append(emale(em1['From'],em1['Date'][:-6],em1['Subject'],em1.get_payload()))
+                if em1.is_multipart() is False:
+                    self.unread.append(emale(em1['From'],em1['Date'][:-6],em1['Subject'],em1.get_payload()))
+                else:
+                    self.unread.append(emale(em1['From'], em1['Date'][:-6], em1['Subject'], em1.get_payload()[0],
+                                             grab_attached(em1)))
                 print('parsed an email')
                 obj.store(em, '-FLAGS', '\\Seen')
         except:
@@ -29,10 +45,15 @@ class DEinbox:
     def num_unread(self):
         return len(self.unread)
 
+   # @staticmethod
+   # def place_unread():
+
+
+
 
 
 class emale:
-    def __init__(self,from_add,Date,subject,body,attachments = None):
+    def __init__(self,from_add,Date,subject,body,attachments = []):
         self.from_add = from_add
         self.body = body
         self.attachments = attachments
